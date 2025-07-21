4171c2ff921e8abed18fea1192c0250e660a1935
danafdr_ <74883985+Danafdr@users.noreply.github.com>
2025-07-21 23:08:20 +0700

    Add src/types/custom-elements.d.ts 1753114095968

---
+++ src/types/custom-elements.d.ts
@@ -0,0 +1,28 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    'portfolio-navigation': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      'data-config'?: string;
+    };
+    'portfolio-hero': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      'data-config'?: string;
+    };
+    'portfolio-about': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      'data-config'?: string;
+    };
+    'portfolio-projects': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      'data-config'?: string;
+    };
+    'portfolio-skills': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      'data-config'?: string;
+    };
+    'portfolio-experience': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      'data-config'?: string;
+    };
+    'portfolio-contact': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      'data-config'?: string;
+    };
+    'portfolio-footer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      'data-config'?: string;
+    };
+  }
+}
